a97eb327deca7f85568f8381d467a432e5481a82
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-06-13 20:25:55 +0900

    Fix by Norminette

--- iterator.h
+++ iterator.h
@@ -1,11 +1,11 @@
 #ifndef ITERATOR_H
-#define ITERATOR_H
+# define ITERATOR_H
 
 typedef struct s_iterator {
-	int		(*next)(struct s_iterator* iter);
-	void*	(*elem)(struct s_iterator* iter);
-	void	(*dispose)(struct s_iterator* iter);
-	void*	istruct;
+	int		(*next)(struct s_iterator*);
+	void	*(*elem)(struct s_iterator*);
+	void	(*dispose)(struct s_iterator*);
+	void	*istruct;
 }	t_iterator;
 
 #endif
